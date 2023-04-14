a118ad29d5399248ca72df4cbb3e4abaa72d16eb
Mohammad <rezaparvizi1994@gmail.com>
2023-04-14 16:53:12 +0330

    feat(openai): impl openai ChatGPT completion

--- cmd/cli/openai.go
+++ cmd/cli/openai.go
@@ -27,3 +27,27 @@ func (c *oaiClients) openaiGptCompletion(ctx context.Context, prompt strings.Bui
 
 	return resp.Choices[0].Text, nil
 }
+
+func (c *oaiClients) openaiGptChatCompletion(ctx context.Context, prompt strings.Builder, maxTokens *int, temp float32) (string, error) {
+	resp, err := c.openAIClient.ChatCompletion(ctx, openai.ChatCompletionRequest{
+		Model: *openAIDeploymentName,
+		Messages: []openai.ChatCompletionRequestMessage{
+			{
+				Role:    userRole,
+				Content: prompt.String(),
+			},
+		},
+		MaxTokens:   *maxTokens,
+		N:           1,
+		Temperature: &temp,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if len(resp.Choices) != 1 {
+		return "", fmt.Errorf("expected choices to be 1 but received: %d", len(resp.Choices))
+	}
+
+	return resp.Choices[0].Message.Content, nil
+}
