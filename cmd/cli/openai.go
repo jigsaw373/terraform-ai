c6ed50d2e10b1e03713eb2fe0b7858b0fb4e59f1
Mohammad <rezaparvizi1994@gmail.com>
2023-04-14 16:52:46 +0330

    feat(openai): impl openai GPT completion

---
+++ cmd/cli/openai.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	openai "github.com/PullRequestInc/go-gpt3"
+	"github.com/ia-ops/terraform-ai/pkg/utils"
+)
+
+func (c *oaiClients) openaiGptCompletion(ctx context.Context, prompt strings.Builder, maxTokens *int, temp float32) (string, error) {
+	resp, err := c.openAIClient.CompletionWithEngine(ctx, *openAIDeploymentName, openai.CompletionRequest{
+		Prompt:      []string{prompt.String()},
+		MaxTokens:   maxTokens,
+		Echo:        false,
+		N:           utils.ToPtr(1),
+		Temperature: &temp,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if len(resp.Choices) != 1 {
+		return "", fmt.Errorf("expected choices to be 1 but received: %d", len(resp.Choices))
+	}
+
+	return resp.Choices[0].Text, nil
+}
