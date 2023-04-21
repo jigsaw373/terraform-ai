6b2bec2dbccd60be66caa6e1c0b207eb4086c82b
Mohammad <rezaparvizi1994@gmail.com>
2023-04-21 23:16:18 +0330

    feat(cli): impl terraform init

---
+++ cmd/cli/init.go
@@ -0,0 +1,75 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/ia-ops/terraform-ai/pkg/terraform"
+	"github.com/ia-ops/terraform-ai/pkg/utils"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+const initSubCommand = "You are a Terraform HCL generator, only generate valid provider Terraform HCL templates."
+
+var errLength = errors.New("invalid length")
+
+func addInit() *cobra.Command {
+	initCmd := &cobra.Command{
+		Use:   "init",
+		Short: "Run terraform init",
+		RunE:  initCommand,
+	}
+
+	return initCmd
+}
+
+func initCommand(_ *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return errors.Wrap(errLength, "prompt must be provided")
+	}
+
+	return initCmd(args)
+}
+
+func initCmd(args []string) error {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	oaiClients, err := newOAIClients()
+	if err != nil {
+		return fmt.Errorf("error new OAI client: %w", err)
+	}
+
+	completion, err := completion(ctx, oaiClients, args, *openAIDeploymentName, initSubCommand)
+	if err != nil {
+		return fmt.Errorf("error completation: %w", err)
+	}
+
+	text := fmt.Sprintf("✨ Attempting to apply the following template: %s", completion)
+	log.Println(text)
+
+	confirmation, err := terraform.GetApplyConfirmation(*requireConfirmation)
+	if err != nil {
+		return fmt.Errorf("error apply confirmation: %w", err)
+	}
+
+	if confirmation {
+		if err = terraform.CheckTemplate(completion); err != nil {
+			return fmt.Errorf("error check template: %w", err)
+		}
+
+		if err = utils.StoreFile("provider.tf", completion); err != nil {
+			return fmt.Errorf("error store file: %w", err)
+		}
+
+		if err = ops.Init(); err != nil {
+			return fmt.Errorf("error run terraform init: %w", err)
+		}
+	}
+
+	return nil
+}
--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -1,25 +1,16 @@
 package cli
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"log"
-	"os"
-	"os/signal"
 	"strconv"
 
-	ter "github.com/ia-ops/terraform-ai/pkg/terraform"
-	"github.com/manifoldco/promptui"
-	"github.com/pkg/errors"
+	"github.com/ia-ops/terraform-ai/pkg/terraform"
 	"github.com/spf13/cobra"
 	"github.com/walles/env"
 )
 
-const (
-	version         = "0.0.1"
-	applySubCommand = "You are a Terraform HCL generator, only generate valid Terraform HCL without provider templates."
-)
+const version = "0.0.1"
 
 var (
 	openAIDeploymentName = flag.String("openai-deployment-name", env.GetOr("OPENAI_DEPLOYMENT_NAME", env.String, "text-davinci-003"), "The deployment name used for the model in OpenAI service.")
@@ -28,11 +19,22 @@ var (
 	azureOpenAIEndpoint  = flag.String("azure-openai-endpoint", env.GetOr("AZURE_OPENAI_ENDPOINT", env.String, ""), "The endpoint for Azure OpenAI service. If provided, Azure OpenAI service will be used instead of OpenAI service.")
 	requireConfirmation  = flag.Bool("require-confirmation", env.GetOr("REQUIRE_CONFIRMATION", strconv.ParseBool, true), "Whether to require confirmation before executing the command. Defaults to true.")
 	sensitivity          = flag.Float64("sensitivity", env.GetOr("SENSITIVITY", env.WithBitSize(strconv.ParseFloat, 64), 0.0), "The sensitivity to use for the model. Range is between 0 and 1. Set closer to 0 if your want output to be more deterministic but less creative. Defaults to 0.0.")
-	errPrompt            = errors.New("invalid prompt")
+	workingDir           = flag.String("working-dir", env.GetOr("WORKING_DIR", env.String, ""), "The path of project that you want to run.")
+	execDir              = flag.String("exec-dir", env.GetOr("WORKING_DIR", env.String, ""), "The path of terraform.")
+	ops                  terraform.Ops
+	err                  error
 )
 
-func InitAndExecute() {
+func InitAndExecute(workDir string, executionDir string) {
 	flag.Parse()
+
+	if *workingDir == "" {
+		workingDir = &workDir
+	}
+
+	if *execDir == "" {
+		execDir = &executionDir
+	}
 
 	if *openAIAPIKey == "" {
 		log.Fatal("Please provide an OpenAI key.")
@@ -44,70 +46,22 @@ func InitAndExecute() {
 }
 
 func RootCmd() *cobra.Command {
+	ops, err = terraform.NewTerraform(*workingDir, *execDir)
+	if err != nil {
+		return nil
+	}
+
 	cmd := &cobra.Command{
 		Use:     "terraform-ai",
 		Version: version,
+		Args:    cobra.MinimumNArgs(1),
 		RunE:    runCommand,
 	}
 
 	cmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 
+	initCmd := addInit()
+	cmd.AddCommand(initCmd)
+
 	return cmd
-}
-
-func runCommand(_ *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		return errors.Wrap(errPrompt, "prompt must be provided")
-	}
-
-	return run(args)
-}
-
-func run(args []string) error {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
-
-	oaiClients, err := newOAIClients()
-	if err != nil {
-		return fmt.Errorf("error run command: %w", err)
-	}
-
-	completion, err := completion(ctx, oaiClients, args, *openAIDeploymentName, applySubCommand)
-	if err != nil {
-		return err
-	}
-
-	text := fmt.Sprintf("✨ Attempting to apply the following template: %s", completion)
-	log.Println(text)
-
-	confirmation, err := getUserConfirmation(*requireConfirmation)
-	if err != nil {
-		return fmt.Errorf("error running select: %w", err)
-	}
-
-	if confirmation {
-		if err = ter.CheckTemplate(completion); err != nil {
-			return fmt.Errorf("error check template: %w", err)
-		}
-	}
-
-	return nil
-}
-
-func getUserConfirmation(requireConfirmation bool) (bool, error) {
-	if !requireConfirmation {
-		return true, nil
-	}
-
-	prompt := promptui.Select{
-		Label: "Would you like to apply this? [Apply/Don't Apply]",
-		Items: []string{"Apply", "Don't Apply"},
-	}
-
-	_, result, err := prompt.Run()
-	if err != nil {
-		return false, fmt.Errorf("error running select: %w", err)
-	}
-
-	return result == "Apply", nil
 }
