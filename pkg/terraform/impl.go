982270bc6fbeec4f4cc022daa37067878b7b64d4
Mohammad <rezaparvizi1994@gmail.com>
2023-04-28 21:50:29 +0330

    feat(terraform): add spinner progress bar

--- pkg/terraform/impl.go
+++ pkg/terraform/impl.go
@@ -3,22 +3,39 @@ package terraform
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/briandowns/spinner"
 )
 
 func (ter *Terraform) Init() error {
+	spin := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+	spin.Start()
+
 	err := ter.Exec.Init(context.Background())
 	if err != nil {
+		spin.Stop()
+
 		return fmt.Errorf("error running Init: %w", err)
 	}
+
+	spin.Stop()
 
 	return nil
 }
 
 func (ter *Terraform) Apply() error {
+	spin := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+	spin.Start()
+
 	err := ter.Exec.Apply(context.Background())
 	if err != nil {
+		spin.Stop()
+
 		return fmt.Errorf("error running Apply: %w", err)
 	}
+
+	spin.Stop()
 
 	return nil
 }
