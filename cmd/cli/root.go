6338dad2b0b8dd63258ba9cf23e7771820897e74
Mohammad <rezaparvizi1994@gmail.com>
2023-05-14 00:37:31 +0330

    fix: change exec-dir var to EXEC_DIR

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -20,7 +20,7 @@ var (
 	requireConfirmation  = flag.Bool("require-confirmation", env.GetOr("REQUIRE_CONFIRMATION", strconv.ParseBool, true), "Whether to require confirmation before executing the command. Defaults to true.")
 	temperature          = flag.Float64("temperature", env.GetOr("TEMPERATURE", env.WithBitSize(strconv.ParseFloat, 64), 0.0), "The temperature to use for the model. Range is between 0 and 1. Set closer to 0 if your want output to be more deterministic but less creative. Defaults to 0.0.")
 	workingDir           = flag.String("working-dir", env.GetOr("WORKING_DIR", env.String, ""), "The path of project that you want to run.")
-	execDir              = flag.String("exec-dir", env.GetOr("WORKING_DIR", env.String, ""), "The path of terraform.")
+	execDir              = flag.String("exec-dir", env.GetOr("EXEC_DIR", env.String, ""), "The path of terraform.")
 	ops                  terraform.Ops
 	err                  error
 )
